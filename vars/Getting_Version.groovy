8c5b74c23ab061cb9c57f5004dc7faac2b3be77c
Sameer A <sksameer4970@gmail.com>
2025-02-20 21:36:19 +0530

    Create Getting_Version.groovy

---
+++ vars/Getting_Version.groovy
@@ -0,0 +1,44 @@
+// vars/versionPrompt.groovy
+#!/usr/bin/env groovy
+
+/**
+ * Prompts the user for a version number with a timeout
+ * If no input is received, automatically increments the patch version
+ *
+ * @param defaultVersion The default version to use if no input is provided
+ * @param timeout The timeout in seconds for user input (default: 30)
+ * @param message The message to display in the input prompt (optional)
+ * @return The selected version (either user input or auto-incremented)
+ */
+def call(Map config = [:]) {
+    def defaultVersion = config.defaultVersion ?: env.DEFAULT_VERSION ?: '1.0.0'
+    def timeoutValue = config.timeout ?: 30
+    def promptMessage = config.message ?: 'Please Provide The Version Of The Web Application'
+    
+    echo "The Default Version is: ${defaultVersion}"
+    
+    try {
+        timeout(time: timeoutValue, unit: 'SECONDS') {
+            // Prompt user to provide a new version number within specified timeout
+            def userVersion = input(
+                message: promptMessage,
+                ok: 'Submit',
+                parameters: [string(defaultValue: defaultVersion, 
+                                    description: 'Provide a new version number', 
+                                    name: 'new_ver')]
+            )
+            
+            // If input is provided, set it as the new version
+            def newVersion = userVersion
+            echo "The New Version Provided is: ${newVersion}"
+            return newVersion
+        }
+    } catch (Exception e) {
+        // On timeout or abort, increment the default version number by 0.0.1
+        def version = defaultVersion.tokenize('.')
+        version[2] = (version[2] as Integer) + 1
+        def newVersion = version.join('.')
+        echo "No Version Provided So The New Version Will Be: ${newVersion}"
+        return newVersion
+    }
+}

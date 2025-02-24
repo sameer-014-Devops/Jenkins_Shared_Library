d21add12277bfff3c15e6d232806735adf86458d
Sameer A <sksameer4970@gmail.com>
2025-02-24 22:32:57 +0530

    Update Docker_Cleanup.groovy

--- vars/Docker_Cleanup.groovy
+++ vars/Docker_Cleanup.groovy
@@ -1,40 +1,24 @@
-def call(String userName, String appName, String newVersion, String defaultVersion) {
-    def credentialsId = config.credentialsId ?: 'dockerhubCredentials'
-    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'dockerhubCredentials_Passwd', usernameVariable: 'dockerhubCredentials_User')]) {
-        try {
-            echo "Checking for the latest Docker image..."
-            def latestImageExists = sh(script: """docker images -q ${env.dockerhubCredentials_User}/${userName}-${appName}-img:latest""", returnStdout: true).trim()
+def call(String dockerUser, String userName, String appName, String newVersion) {
+    script {
+        def latestTag = "${dockerUser}/${userName}-${appName}-img:latest"
+        def versionedTag = "${dockerUser}/${userName}-${appName}-img:${newVersion}"
 
-            if (latestImageExists) {
-                echo 'Docker Image with the latest tag already exists. Removing the existing image...'
-                sh """docker rmi -f ${env.dockerhubCredentials_User}/${userName}-${appName}-img:latest"""
-            } else {
-                echo 'Docker Image with the latest tag does not exist...'
-            }
+        // Check and remove latest image if it exists
+        def latestImageExists = sh(script: "docker images -q ${latestTag}", returnStdout: true).trim()
+        if (latestImageExists) {
+            echo "Docker Image with the latest tag already exists. Removing the existing image..."
+            sh "docker rmi -f ${latestTag}"
+        } else {
+            echo "Docker Image with the latest tag does not exist."
+        }
 
-            echo "Checking for the versioned Docker image..."
-            def versionedImageExists = sh(script: """docker images -q ${env.dockerhubCredentials_User}/${userName}-${appName}-img:${newVersion}""", returnStdout: true).trim()
-
-            if (versionedImageExists) {
-                echo "Docker Image with the ${newVersion} version already exists. Removing the existing image..."
-                sh """docker rmi -f ${env.dockerhubCredentials_User}/${userName}-${appName}-img:${newVersion}"""
-            } else {
-                echo "Docker Image with the ${newVersion} version does not exist..."
-            }
-
-            echo "Checking for the default versioned Docker image..."
-            def defaultVersionedImageExists = sh(script: """docker images -q ${env.dockerhubCredentials_User}/${userName}-${appName}-img:${defaultVersion}""", returnStdout: true).trim()
-
-            if (defaultVersionedImageExists) {
-                echo "Docker Image with the ${defaultVersion} version already exists. Removing the existing image..."
-                sh """docker rmi -f ${env.dockerhubCredentials_User}/${userName}-${appName}-img:${defaultVersion}"""
-            } else {
-                echo "Docker Image with the ${defaultVersion} version does not exist..."
-            }
-        } catch (Exception e) {
-            echo "############################## Docker Cleanup is FAILED ##############################"
-            echo "Error: ${e.message}"
-            throw e
+        // Check and remove versioned image if it exists
+        def versionedImageExists = sh(script: "docker images -q ${versionedTag}", returnStdout: true).trim()
+        if (versionedImageExists) {
+            echo "Docker Image with the specified version (${newVersion}) already exists. Removing the existing image..."
+            sh "docker rmi -f ${versionedTag}"
+        } else {
+            echo "Docker Image with the specified version (${newVersion}) does not exist."
         }
     }
 }

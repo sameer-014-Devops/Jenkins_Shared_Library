30ca4d029b15f9655d46692975acdc06ee7cec1d
Sameer A <sksameer4970@gmail.com>
2025-02-24 15:55:18 +0530

    Update SCM_CheckOut.groovy

--- vars/SCM_CheckOut.groovy
+++ vars/SCM_CheckOut.groovy
@@ -1,7 +1,7 @@
 def call(String GitURL, String GitBranch) {
     try {
         echo "Cloning repository from ${GitURL} on branch ${GitBranch}"
-        git url: "${GitURL}", branch: "${GitBranch}"
+        git url:"${GitURL}",branch:"${GitBranch}"
         echo "Repository cloned successfully."
     } catch (Exception e) {
         echo "Failed to clone repository: ${e.message}"

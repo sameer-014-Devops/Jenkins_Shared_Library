040782e4f4fd3d72144cf69b556b3f5e4ae531d6
Sameer A <sksameer4970@gmail.com>
2025-02-24 22:36:16 +0530

    Update Docker_Cleanup.groovy

--- vars/Docker_Cleanup.groovy
+++ vars/Docker_Cleanup.groovy
@@ -1,7 +1,8 @@
-def call(String dockerUser, String userName, String appName, String newVersion) {
+def call(String dockerUser, String userName, String appName, String newVersion, String defaultVersion) {
     script {
         def latestTag = "${dockerUser}/${userName}-${appName}-img:latest"
         def versionedTag = "${dockerUser}/${userName}-${appName}-img:${newVersion}"
+        def defaultTag = "${dockerUser}/${userName}-${appName}-img:${defaultVersion}"
 
         // Check and remove latest image if it exists
         def latestImageExists = sh(script: "docker images -q ${latestTag}", returnStdout: true).trim()
@@ -19,6 +20,15 @@ def call(String dockerUser, String userName, String appName, String newVersion) 
             sh "docker rmi -f ${versionedTag}"
         } else {
             echo "Docker Image with the specified version (${newVersion}) does not exist."
+        }
+
+        // Check and remove versioned image if it exists
+        def defaultversionedImageExists = sh(script: "docker images -q ${defaultTag}", returnStdout: true).trim()
+        if (defaultversionedImageExists) {
+            echo "Docker Image with the specified version (${defaultVersion}) already exists. Removing the existing image..."
+            sh "docker rmi -f ${defaultTag}"
+        } else {
+            echo "Docker Image with the specified version (${defaultVersion}) does not exist."
         }
     }
 }

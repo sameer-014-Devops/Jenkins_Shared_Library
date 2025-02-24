18f87457441dc5d41aecd4bdd1920776eea47f59
Sameer A <sksameer4970@gmail.com>
2025-02-24 23:54:46 +0530

    Update Docker_Cleanup.groovy

--- vars/Docker_Cleanup.groovy
+++ vars/Docker_Cleanup.groovy
@@ -5,28 +5,28 @@ def call(String dockerUser, String userName, String appName, String newversion, 
         def defaultversionTag = "${dockerUser}/${userName}-${appName}-img:${defaultVersion}"
 
         // Check and Remove latest image if it exists
-        def latestImageExists = sh(script: "docker image ls | grep ${latestTag}", returnStatus: true) 
+        def latestImageExists = sh(script: 'docker image ls | grep "$LATEST_TAG"', returnStatus: true)
         if(latestImageExists == 0){
             echo "Latest image found () ${latestTag} ) Removing it"
-            sh "docker rmi ${latestTag}"
+            sh 'docker rmi "${latestTag}"'
         } else {
             echo "No latest image found"
         }
 
         // Check and Remove new version image if it exists
-        def newversionImageExists = sh(script: "docker image ls | grep ${newversionTag}", returnStatus: true)
+        def newversionImageExists = sh(script: 'docker image ls | grep "${newversionTag}"', returnStatus: true)
         if(newversionImageExists == 0){
             echo "New version image found () ${newversionTag} ) Removing it"
-            sh "docker rmi ${newversionTag}"
+            sh 'docker rmi "${newversionTag}"'
         } else {
             echo "No new version image found"
         }
 
         // Check and Remove default version image if it exists
-        def defaultversionImageExists = sh(script: "docker image ls | grep ${defaultversionTag}", returnStatus: true)
+        def defaultversionImageExists = sh(script: 'docker image ls | grep "${defaultversionTag}"', returnStatus: true)
         if(defaultversionImageExists == 0){
             echo "Default version image found () ${defaultversionTag} ) Removing it"
-            sh "docker rmi ${defaultversionTag}"
+            sh 'docker rmi "${defaultversionTag}"'
         } else {
             echo "No default version image found"
         }

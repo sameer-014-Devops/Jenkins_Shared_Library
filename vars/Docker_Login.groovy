fa6150e28cb4f252bcae1c3fa9527a50d153c0ff
Sameer A <sksameer4970@gmail.com>
2025-02-25 23:35:17 +0530

    Update Docker_Login.groovy

--- vars/Docker_Login.groovy
+++ vars/Docker_Login.groovy
@@ -1,40 +1,13 @@
-// vars/dockerHubLogin.groovy
-#!/usr/bin/env groovy
-
-/**
- * Performs Docker Hub login and verifies the success
- *
- * @param credentialsId The Jenkins credentials ID containing Docker Hub username/password (optional)
- * @param verifyLogin Whether to verify the login success (default: true)
- * @return Boolean indicating if login was successful
- */
-def call(Map config = [:]) {
-    def credentialsId = config.credentialsId ?: 'dockerhubCredentials'
-    def verifyLogin = config.verifyLogin != null ? config.verifyLogin : true
-    def dockerLogin = false
+def call(String dockerUser, String dockerPasswd){
     
-    echo '**********Login to Docker Hub**********'
-    
-    withCredentials([usernamePassword(credentialsId: credentialsId, 
-                                       passwordVariable: 'dockerhubCredentials_Passwd', 
-                                       usernameVariable: 'dockerhubCredentials_User')]) {
-        sh "echo \$dockerhubCredentials_Passwd | docker login -u \$dockerhubCredentials_User --password-stdin"
+    script{
         
-        if (verifyLogin) {
-            def loginStatus = sh(script: "docker info | grep -i \"Username: \$dockerhubCredentials_User\"", 
-                                 returnStatus: true)
-            
-            if (loginStatus == 0) {
-                echo '*********Docker Hub Login SUCCESSFUL*********'
-                dockerLogin = true
-            } else {
-                error '*********Docker Hub Login FAILED*********'
-            }
+        sh "echo ${dockerPasswd} | docker login -u ${dockerUser} --password-stdin"
+        if (sh(script: "docker info | grep -i 'Username: ${dockerUser}'", returnStatus: true) == 0) {
+            echo "********* DockerHub Login SUCCESSFUL with User ${dockerUser} *********"
         } else {
-            // If verification is disabled, assume success based on login command
-            dockerLogin = true
+            error '********* DockerHub Login FAILED *********'
         }
+        
     }
-    
-    return dockerLogin
 }

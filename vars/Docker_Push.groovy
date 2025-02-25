b127055a8f32abcdaf6b1e26a20cd41142b64ea4
Sameer A <sksameer4970@gmail.com>
2025-02-25 23:44:52 +0530

    Update Docker_Push.groovy

--- vars/Docker_Push.groovy
+++ vars/Docker_Push.groovy
@@ -1,55 +1,16 @@
-// vars/dockerPushImage.groovy
-#!/usr/bin/env groovy
+def call(String dockerUser, String userName, String appName, String tierName, String newversion){
+    
+    script{
+        def latestTag = "${dockerUser}/${userName}-${appName}-${tierName}-img:latest"
+        def newversionTag = "${dockerUser}/${userName}-${appName}-${tierName}-img:${newversion}"
 
-/**
- * Pushes Docker images to Docker Hub with version and latest tags
- *
- * @param userName The user name part of the image name
- * @param appName The application name part of the image name
- * @param version The version of the image to push
- * @param pushLatest Whether to also push the latest tag (default: true)
- * @param credentialsId The Jenkins credentials ID for Docker Hub (optional)
- * @return Boolean indicating if push was successful
- */
-def call(Map config = [:]) {
-    def userName = config.userName ?: error("userName parameter is required")
-    def appName = config.appName ?: error("appName parameter is required")
-    def version = config.newVersion ?: error("newVersion parameter is required")
-    def pushLatest = config.pushLatest != null ? config.pushLatest : true
-    def credentialsId = config.credentialsId ?: "dockerhubCredentials"
-    def pushSuccess = false
-    
-    withCredentials([usernamePassword(credentialsId: credentialsId, 
-                                       passwordVariable: 'dockerhubCredentials_Passwd', 
-                                       usernameVariable: 'dockerhubCredentials_User')]) {
-        
-        def baseImageName = "${env.dockerhubCredentials_User}/${userName}-${appName}-img"
-        
-        // Push versioned image
-        echo "********Pushing Docker Image With The Version ${version}********"
-        sh """docker push ${baseImageName}:${version}"""
-        
-        // Push latest tag if requested
-        if (pushLatest) {
-            echo "********Pushing Docker Image With The Tag 'latest'********"
-            sh """docker push ${baseImageName}:latest"""
-        }
-        
-        // Verify the push was successful
-        // Note: This is a simple verification that the image still exists locally
-        // A more robust solution would check the Docker Hub API
-        def imagePushed = sh(
-            script: """docker images -q ${baseImageName}:${version}""", 
-            returnStdout: true
-        ).trim()
-        
-        if (imagePushed) {
-            echo '*********Docker Image Push SUCCESSFUL*********'
-            pushSuccess = true
-        } else {
-            error '*********Docker Image Push FAILED*********'
-        }
+        // Pushing to DockerHub Repository
+        echo "******** Pushing Docker Image ${newversionTag} To Repository ********"
+        sh "docker push ${newversionTag}"
+        echo "******** Pushing Docker Image ${newversionTag} Completed ********"
+
+        echo "******** Pushing Docker Image ${latestTag} To Repository ********"
+        sh "docker push ${latestTag}"
+        echo "******** Pushing Docker Image ${latestTag} Completed ********"
     }
-    
-    return pushSuccess
 }

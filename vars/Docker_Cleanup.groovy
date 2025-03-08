c0edcd71f9682459134a524c0fb47ff5ae38ba1a
Sameer A <sksameer4970@gmail.com>
2025-03-08 08:02:18 +0530

    Update Docker_Cleanup.groovy

--- vars/Docker_Cleanup.groovy
+++ vars/Docker_Cleanup.groovy
@@ -2,7 +2,7 @@ def call(){
     
     script {
         
-        def latestImageExistsOne = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:latest", returnStatus: true).trim()
+        def latestImageExistsOne = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:latest", returnStatus: true)
         if (latestImageExistsOne == 0) {
             echo "Docker Image ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:latest already exists. Removing the existing image..."
             sh "docker rmi -f ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:latest"
@@ -10,7 +10,7 @@ def call(){
             echo "Docker ${env.tierOne} Image with the latest tag does not exist."
         }
 
-        def latestImageExistsTwo = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:latest", returnStatus: true).trim()
+        def latestImageExistsTwo = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:latest", returnStatus: true)
         if (latestImageExistsTwo == 0) {
             echo "Docker Image $latestImageExistsTwo with the latest tag already exists. Removing the existing image..."
             sh "docker rmi -f ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:latest"
@@ -18,7 +18,7 @@ def call(){
             echo "Docker ${env.tierTwo} Image with the latest tag does not exist."
         }
 
-        def defaultVersionExistsOne = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.defaultVersion}", returnStatus: true).trim()
+        def defaultVersionExistsOne = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.defaultVersion}", returnStatus: true)
         if (defaultVersionExistsOne == 0) {
             echo "Docker Image ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.defaultVersion} already exists. Removing the existing image..."
             sh "docker rmi -f ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.defaultVersion}"
@@ -26,7 +26,7 @@ def call(){
             echo "Docker ${env.tierOne} Image with the default version does not exist."
         }
 
-        def defaultVersionExistsTwo = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.defaultVersion}", returnStatus: true).trim()
+        def defaultVersionExistsTwo = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.defaultVersion}", returnStatus: true)
         if (defaultVersionExistsTwo == 0) {
             echo "Docker Image ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.defaultVersion} already exists. Removing the existing image..."
             sh "docker rmi -f ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.defaultVersion}"
@@ -34,7 +34,7 @@ def call(){
             echo "Docker ${env.tierTwo} Image with the default version does not exist."
         }
 
-        def newVersionExistsOne = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.newVersion}", returnStatus: true).trim()
+        def newVersionExistsOne = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.newVersion}", returnStatus: true)
         if (newVersionExistsOne == 0) {
             echo "Docker Image ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.newVersion} already exists. Removing the existing image..."
             sh "docker rmi -f ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierOne}-img:${env.newVersion}"
@@ -42,7 +42,7 @@ def call(){
             echo "Docker ${env.tierOne} Image with the new version does not exist."
         }
 
-        def newVersionExistsTwo = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.newVersion}", returnStatus: true).trim()
+        def newVersionExistsTwo = sh(script: "docker images -q ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.newVersion}", returnStatus: true)
         if (newVersionExistsTwo == 0) {
             echo "Docker Image ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.newVersion} already exists. Removing the existing image..."
             sh "docker rmi -f ${env.dockerhubUser}/${env.userName}-${env.appName}-${env.tierTwo}-img:${env.newVersion}"

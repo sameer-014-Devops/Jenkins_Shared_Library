876d51f63c8e8c81366e8351a48a6f35a9200a99
Sameer A <sksameer4970@gmail.com>
2025-03-08 09:59:55 +0530

    Update Docker_Login.groovy

--- vars/Docker_Login.groovy
+++ vars/Docker_Login.groovy
@@ -7,7 +7,7 @@ def call(){
         sh "echo $password | docker login -u $userName --password-stdin"
 
         // Check if the login is successful
-        if (sh(script: 'docker info | grep -i "Username: ${userName}"', returnStatus: true) == 0) {
+        if (sh(script: "docker info | grep -i 'Username: $userName'", returnStatus: true) == 0) {
             echo '***********DockerHub login SUCCESSFUL***********'
         } else {
             error '**********DockerHub login FAILED**********'

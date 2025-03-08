ab47f312c594bdab55a89800f0082986b1e7e25d
Sameer A <sksameer4970@gmail.com>
2025-03-08 10:19:33 +0530

    Update Dependency_Check.groovy

--- vars/Dependency_Check.groovy
+++ vars/Dependency_Check.groovy
@@ -1,12 +1,12 @@
 def call() {
     
     try {
-        echo "########## Starting Dependency Check ##########"
+        echo "**********Starting Dependency Check**********"
         dependencyCheck additionalArguments: "--scan ./", odcInstallation: 'OWASP'
         dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
-        echo "########## Dependency Check Completed ##########"
+        echo "**********Dependency Check Completed**********"
     } catch (Exception e) {
-        echo "########## Dependency Check Failed ##########"
+        echo "**********Dependency Check Failed**********"
         echo "Error: ${e.message}"
         throw e
     }

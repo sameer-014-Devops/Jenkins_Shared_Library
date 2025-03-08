813ef8e63f543e63a718071d1bfa2b0f4417e75d
Sameer A <sksameer4970@gmail.com>
2025-03-08 10:36:21 +0530

    Update and rename Docker_Build.groovy to Docker_Build_One.groovy

--- vars/Docker_Build_One.groovy
+++ vars/Docker_Build_One.groovy
@@ -4,8 +4,6 @@ def call(){
 
         def latestImageOne = env.dockerhubUser + "/" + env.userName + "-" + env.appName + "-" + env.tierOne + "-img:latest"
         def newImageOne = env.dockerhubUser + "/" + env.userName + "-" + env.appName + "-" + env.tierOne + "-img:" + env.newVersion
-        def latestImageTwo = env.dockerhubUser + "/" + env.userName + "-" + env.appName + "-" + env.tierTwo + "-img:latest"
-        def newImageTwo = env.dockerhubUser + "/" + env.userName + "-" + env.appName + "-" + env.tierTwo + "-img:" + env.newVersion
 
         // Log Out the DockerHub Account
         sh "docker logout"
@@ -15,17 +13,10 @@ def call(){
         // Tag the Docker Image for Tier One
         echo "**********Tagging the Docker Image $newImageOne**********"
         sh "docker tag $newImageOne $latestImageOne"
-        // Build the Docker Image for Tier Two
-        echo "**********Building the Docker Image $newImageTwo**********"
-        sh "docker build -t $newImageTwo ."
-        // Tag the Docker Image for Tier Two
-        echo "**********Tagging the Docker Image $newImageTwo**********"
-        sh "docker tag $newImageTwo $latestImageTwo"
         echo "**********Docker Images are Built and Tagged Successfully**********"
         
         def latestImageExistsOne = sh(script: "docker images -q $latestImageOne", returnStdout: true)
-        def latestImageExistsTwo = sh(script: "docker images -q $latestImageTwo", returnStdout: true)
-        if (latestImageExistsOne && latestImageExistsTwo) {
+        if (latestImageExistsOne) {
             echo "**********Docker Images are Built and Tagged Successfully**********" 
         } else {
             error "**********Docker Images are NOT Built and Tagged**********"

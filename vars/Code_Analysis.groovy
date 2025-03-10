a79016e5ce15531ab349d7181f57a4989586f981
Sameer A <sksameer4970@gmail.com>
2025-03-10 11:30:20 +0530

    Update Code_Analysis.groovy

--- vars/Code_Analysis.groovy
+++ vars/Code_Analysis.groovy
@@ -2,9 +2,12 @@ def call(){
 
     script {
 
+        def SonarQubeAPI = env.sonarName
         def Projectname = env.appName
         def ProjectKey = env.userName
-        
-        sh "sonar-scanner -Dsonar.projectName=$Projectname -Dsonar.projectKey=$ProjectKey -X"
+
+        withSonarQubeEnv("$SonarQubeAPI") {
+            sh "sonar-scanner -Dsonar.projectName=$Projectname -Dsonar.projectKey=$ProjectKey -X"
+        }
     }
 }
